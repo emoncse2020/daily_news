955e099047efb85b0f96401d121d490c22f4ccba
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-29 00:55:47 +0600

    login register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,15 @@
+import { Link } from "react-router";
+
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
       <div className="px-6 py-4">
-        <div className="flex justify-center mx-auto">
-          <img
-            className="w-auto h-7 sm:h-8"
-            src="https://merakiui.com/images/logo.svg"
-            alt="Logo"
-          />
-        </div>
-
         <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">
           Welcome Back
         </h3>
@@ -18,13 +18,14 @@ const Login = () => {
           Login or create account
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="w-full mt-4">
             <input
               className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg 
               dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 
               dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
               type="email"
+              name="email"
               placeholder="Email Address"
               aria-label="Email Address"
             />
@@ -36,6 +37,7 @@ const Login = () => {
               dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 
               dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
               type="password"
+              name="password"
               placeholder="Password"
               aria-label="Password"
             />
@@ -66,12 +68,12 @@ const Login = () => {
           Don't have an account?{" "}
         </span>
 
-        <a
-          href="#"
+        <Link
+          to={"/auth/register"}
           className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
         >
           Register
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 const Register = () => {
   return (
     <div className="flex my-24 justify-center items-center">
@@ -14,12 +16,13 @@ const Register = () => {
             <input
               type="text"
               id="username"
+              name="name"
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
 
           {/* Password Input */}
-          <div className="mt-4">
+          {/* <div className="mt-4">
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
@@ -40,7 +43,7 @@ const Register = () => {
               id="password"
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
-          </div>
+          </div> */}
 
           {/* Sign In Button */}
           <div className="mt-6">
@@ -48,7 +51,7 @@ const Register = () => {
               type="submit"
               className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
             >
-              Sign In
+              Sign Up
             </button>
           </div>
         </form>
@@ -75,7 +78,7 @@ const Register = () => {
             <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
               <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
             </svg>
-            <span className="hidden mx-2 sm:inline">Sign in with Google</span>
+            <span className="hidden mx-2 sm:inline">Sign Up with Google</span>
           </button>
 
           {/* Twitter/X Button (Assumed from Icon) */}
@@ -91,13 +94,13 @@ const Register = () => {
 
         {/* Sign Up Link */}
         <p className="mt-8 text-xs font-light text-center text-gray-400">
-          Don't have an account?{" "}
-          <a
-            href="#"
+          Already have an account?{" "}
+          <Link
+            to={"/auth/login"}
             className="font-medium text-gray-700 dark:text-gray-200 hover:underline"
           >
-            Create One
-          </a>
+            Sign In
+          </Link>
         </p>
       </div>
     </div>
